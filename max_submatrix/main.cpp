340710e01c76b5a4b466fbc76963d3fe90854d8f
Wei <10310932@std.tcfsh.tc.edu.tw>
2017-11-16 11:33:01 +0800

    Second commit

--- max_submatrix/main.cpp
+++ max_submatrix/main.cpp
@@ -5,8 +5,19 @@ using namespace std;
 int main()
 {
     int width=0, length=0;
-    while(cin>>width, length){
-
+    while(cin>>width){
+        cin>>length;
+        int matrix[width][length];
+        for(int i=0; i<width; i++)
+            for(int j=0; j<length; j++)
+                cin>>matrix[i][j];
+        for(int i=0; i<width; i++){
+            for(int j=0; j<length; j++){
+                cout<<matrix[i][j]<<"\t";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
     }
     return 0;
 }
